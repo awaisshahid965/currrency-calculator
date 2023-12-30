45ab3e9c2476f52391575042b0578d21eddf1def
Muhammad Awais <awaisshahid965@gmail.com>
2023-12-30 22:47:02 +0500

    chore: optimized prettier

--- client/.lintstagedrc.js
+++ client/.lintstagedrc.js
@@ -1,10 +1,11 @@
-const path = require("path");
+const path = require('path')
 
 const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(" --file ")}`;
+  `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`
+
+const buildPrettierCommand = (filenames) =>
+  `prettier --write ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')}`
 
 module.exports = {
-  "*.{ts,tsx}": [buildEslintCommand, "npm run format"],
-};
+  '*.{ts,tsx}': [buildEslintCommand, buildPrettierCommand],
+}
--- server/.lintstagedrc.js
+++ server/.lintstagedrc.js
@@ -1,10 +1,11 @@
-const path = require("path");
+const path = require('path')
 
 const buildEslintCommand = (filenames) =>
-  `eslint --fix ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(" ")}`;
+  `eslint --fix ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')}`
+
+const buildPrettierCommand = (filenames) =>
+  `prettier --write ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')}`
 
 module.exports = {
-  "*.ts": [buildEslintCommand, "npm run format"],
-};
+  '*.ts': [buildEslintCommand, buildPrettierCommand],
+}
